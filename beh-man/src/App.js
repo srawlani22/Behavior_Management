6fa7c8886d675480094bf03403d65d231a2d7835
Sean Guba <sguba22@my.whitworth.edu>
2022-03-01 10:17:07 -0800

    From the React Default App, Added our own image as well as some other Front End Things

--- beh-man/src/App.js
+++ beh-man/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import logo from './WhitLogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src= {logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Behavioral Management Project
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click to Begin
         </a>
       </header>
     </div>
